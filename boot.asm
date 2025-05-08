905e74bb49f9d5b3e22336fb3c05e54bb21e9610
Kross1de <awesome.hubygu@yandex.ru>
2025-05-08 10:27:48 +0600

    Hello, world

--- boot.asm
+++ boot.asm
@@ -8,12 +8,25 @@ start:
       mov es, ax 
       mov ax, 0x03
       int 0x10
+      mov si, message
+      call puts
+      call hang
+
+puts:
       mov ah, 0x0e
-      mov al, 'A'
+      .putc:
+      lodsb
+      cmp al, 0 
+      je .done
       int 0x10
+      jmp .putc
+      .done:
+      ret
 
 hang:
       jmp hang
+
+message: db 'hello, world', 0
 
 times 510-($-$$) db 0  
       dw 0aa55h
