c3cfef8c3f48ec5a823744643bb2e52268420c5c
Kross1de <awesome.hubygu@yandex.ru>
2025-05-08 14:11:02 +0600

    Some restructure & snake game

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Netwide16
 16BIT operating system written in NASM and based on bare bones from https://osdev.netlify.app/x16/mini-kernel/lesson1.html
+
+# Features
+- Netwide16 can draw graphics (12h VM)
+- Snake game (CREDITS: https://github.com/PRoX2011/x16-PRos/blob/main/src/snake.asm)
+- idk cool shell
+
+# TODO
+- [ ] FAT16
+- [ ] More games
+- [ ] Add programs with graphic
--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-nasm -f bin boot.asm -o boot.bin
-nasm -f bin kernel.asm -o kernel.bin
-nasm -f bin program.asm -o program.bin
-dd if=/dev/zero of=image.img bs=512 count=16
-dd if=boot.bin of=image.img conv=notrunc
-dd if=kernel.bin of=image.img bs=512 seek=1 conv=notrunc
-dd if=program.bin of=image.img bs=512 seek=5 conv=notrunc
-qemu-system-i386 -hda image.img
+nasm -f bin src/boot.asm -o bin/boot.bin
+nasm -f bin src/kernel.asm -o bin/kernel.bin
+nasm -f bin src/snake.asm -o bin/snake.bin
+nasm -f bin src/program.asm -o bin/program.bin
+dd if=/dev/zero of=bin/image.img bs=512 count=16
+dd if=bin/boot.bin of=bin/image.img conv=notrunc
+dd if=bin/kernel.bin of=bin/image.img bs=512 seek=1 conv=notrunc
+dd if=bin/program.bin of=bin/image.img bs=512 seek=5 conv=notrunc
+dd if=bin/snake.bin of=bin/image.img bs=512 seek=6 conv=notrunc
+qemu-system-i386 -hda bin/image.img
